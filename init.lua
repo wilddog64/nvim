1b9510dcc9428c126d8bff0aa62cedee9f236404
chengkai liang <chengkli@cisco.com>
2022-03-19 08:53:00 -0700

    add color scheme

--- init.lua
+++ init.lua
@@ -1,4 +1,17 @@
+-- setup tab expansion
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
+
+require('packer').startup(function(use)
+  -- allow packer to manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- colorscheme
+  use 'joshdick/onedark.vim'
+end)
+
+vim.opt.signcolumn = 'yes'
+vim.opt.termguicolors = true
+pcall(vim.cmd, 'colorscheme onedark')
