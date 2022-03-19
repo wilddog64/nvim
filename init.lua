d56f3d3f9fd91c91cd67b6313dc5863a67af8e1d
chengkai liang <chengkli@cisco.com>
2022-03-19 15:19:33 -0700

    move plugins to its own file and directory

--- init.lua
+++ init.lua
@@ -4,67 +4,8 @@ vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 
-require('packer').startup(function(use)
-  -- allow packer to manage itself
-  use 'wbthomason/packer.nvim'
 
-  use 'jiangmiao/auto-pairs'
-  use 'airblade/vim-gitgutter'
-  use 'benmills/vimux'
-  use 'christoomey/vim-tmux-navigator'
-  use 'dbakker/vim-projectroot'
-  use 'ervandew/supertab'
-  use 'fatih/vim-go'
-  use 'hashivim/vim-hashicorp-tools'
-  use 'hashivim/vim-terraform'
-  use 'juliosueiras/vim-terraform-completion'
-  use 'othree/xml.vim'
-  use 'vim-scripts/taglist.vim'
-  use 'majutsushi/tagbar'
-  use 'sjl/gundo.vim'
-  use 'terryma/vim-multiple-cursors'
-  use 'tmhedberg/matchit'
-  use 'tmux-plugins/vim-tmux'
-  use 'tmux-plugins/vim-tmux-focus-events'
-  use 'tpope/vim-endwise'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-repeat'
-  use 'tpope/vim-surround'
-  use 'tpope/vim-unimpaired'
-  use 'szw/vim-maximizer'
-  use 'corntrace/bufexplorer'
-  use 'nvim-treesitter/nvim-treesitter'
-
-  use {
-    'ojroques/nvim-lspfuzzy',
-    requires = {
-      {'junegunn/fzf'},
-      {'junegunn/fzf.vim'},
-    }
-  }
-
-  -- LSP
-    use {
-      'VonHeikemen/lsp-zero.nvim',
-      requires = {
-        -- LSP Support
-        {'neovim/nvim-lspconfig'},
-        {'williamboman/nvim-lsp-installer'},
-
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},
-        {'hrsh7th/cmp-buffer'},
-        {'hrsh7th/cmp-path'},
-        {'saadparwaiz1/cmp_luasnip'},
-        {'hrsh7th/cmp-nvim-lsp'},
-        {'hrsh7th/cmp-nvim-lua'},
-
-        -- Snippets
-        {'L3MON4D3/LuaSnip'},
-        {'rafamadriz/friendly-snippets'},
-      }
-  }
-end)
+require('ckm.plugins')
 
 -- add lsp configuration
 local lsp = require('lsp-zero')
