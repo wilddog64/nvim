ac8c3a23439ecbed3a43fbc0346d0c9befa9c2c3
chengkai liang <chengkli@cisco.com>
2022-03-20 13:56:27 -0700

    disable sourcing keymaps.vim

--- init.lua
+++ init.lua
@@ -17,9 +17,9 @@ require 'lspfuzzy'.setup{}
 
 vim.cmd([[
   source ~/.config/nvim/settings/general.vim
---  source ~/.config/nvim/settings/keymaps.vim
   source ~/.config/nvim/settings/functions.vim
   source ~/.config/nvim/settings/supertab.vim
   source ~/.config/nvim/settings/gundo.vim
   source ~/.config/nvim/settings/deoplete.vim
 ]])
+-- source ~/.config/nvim/settings/keymaps.vim
