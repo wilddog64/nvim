53b064844d86d50d74653cc1c7e3ed05aeb399db
chengkai liang <chengkli@cisco.com>
2022-03-21 21:47:14 -0700

    refactor plugins.lua to handle auto plugin installation

--- lua/ckm/plugins.lua
+++ lua/ckm/plugins.lua
@@ -1,8 +1,46 @@
-local packer = require('packer')
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
 return packer.startup(function(use)
   -- allow packer to manage itself
   use 'wbthomason/packer.nvim'
-
   use 'jiangmiao/auto-pairs'
   use 'airblade/vim-gitgutter'
   use 'benmills/vimux'
@@ -29,11 +67,11 @@ return packer.startup(function(use)
   use 'szw/vim-maximizer'
   use 'corntrace/bufexplorer'
   use 'nvim-treesitter/nvim-treesitter'
-  use {'shougo/deoplete-lsp'}
-  use {'shougo/deoplete.nvim'}
-  use {'neovim/nvim-lspconfig'}
-  use {'rhysd/clever-f.vim'}
-  use {'kyazdani42/nvim-tree.lua'}
+  use 'shougo/deoplete-lsp'
+  use 'shougo/deoplete.nvim'
+  use 'neovim/nvim-lspconfig'
+  use 'rhysd/clever-f.vim'
+  use 'kyazdani42/nvim-tree.lua'
 
   use {
     'kyazdani42/nvim-tree.lua',
@@ -72,4 +110,8 @@ return packer.startup(function(use)
       {'rafamadriz/friendly-snippets'},
     }
   }
+
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
 end)
