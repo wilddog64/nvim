63cdfab7d3532a4322a5f860fc86a0f9304ed2b6
chengkai liang <chengkli@cisco.com>
2022-03-24 07:04:44 -0700

    add more setup for apprentice color scheme

--- lua/usr/colors/apprentice.lua
+++ lua/usr/colors/apprentice.lua
@@ -1,1 +1,54 @@
-require('apprentice').setup()
+vim.o.background = "dark" -- or "light" for light mode
+
+-- Load and setup function to choose plugin and language highlights
+require('lush')(require('apprentice').setup({
+  plugins = {
+    "buftabline",
+    "coc",
+    "cmp", -- nvim-cmp
+    "fzf",
+    "gitgutter",
+    "gitsigns",
+    "lsp",
+    "lspsaga",
+    "nerdtree",
+    "netrw",
+    "nvimtree",
+    "neogit",
+    "packer",
+    "signify",
+    "startify",
+    "syntastic",
+    "telescope",
+    "treesitter"
+  },
+  langs = {
+    "c",
+    "clojure",
+    "coffeescript",
+    "csharp",
+    "css",
+    "elixir",
+    "golang",
+    "haskell",
+    "html",
+    "java",
+    "js",
+    "json",
+    "jsx",
+    "lua",
+    "markdown",
+    "moonscript",
+    "objc",
+    "ocaml",
+    "purescript",
+    "python",
+    "ruby",
+    "rust",
+    "scala",
+    "typescript",
+    "viml",
+    "xml"
+  }
+}))
+
