80dcc54745dfe984c15d2e0a88c11d65c7916572
chengkai liang <chengkli@cisco.com>
2022-04-08 04:11:49 +0000

    setup complete options

--- lua/usr/options.lua
+++ lua/usr/options.lua
@@ -29,7 +29,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
-
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
 -- what we want to store in a session file
 vim.opt.ssop = {
