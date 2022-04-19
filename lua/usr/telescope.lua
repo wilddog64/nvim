98b6c202dba542e3e811eecc61114f6f2fbf01ae
chengkai liang <chengkli@cisco.com>
2022-04-19 06:38:41 -0700

    sort mapping keys

--- lua/usr/telescope.lua
+++ lua/usr/telescope.lua
@@ -99,15 +99,15 @@ telescope.load_extension('urlview')
 
 vim.cmd([[
   " Using Lua functions
+  nnoremap <leader>cf <cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>
+  nnoremap <leader>fa <cmd>lua require('telescope.builtin').autocommands()<cr>
+  nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
+  nnoremap <leader>fc <cmd>lua require('telescope.builtin').commands()<cr>
   nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
   nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
-  nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
   nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
-  nnoremap <leader>fs <cmd>lua require('telescope.builtin').grep_string()<cr>
-  nnoremap <leader>fc <cmd>lua require('telescope.builtin').commands()<cr>
+  nnoremap <leader>fk <cmd>lua require('telescope.builtin').keymaps()<cr>
   nnoremap <leader>fm <cmd>lua require('telescope.builtin').marks()<cr>
   nnoremap <leader>fr <cmd>lua require('telescope.builtin').registers()<cr>
-  nnoremap <leader>fk <cmd>lua require('telescope.builtin').keymaps()<cr>
-  nnoremap <leader>fa <cmd>lua require('telescope.builtin').autocommands()<cr>
-  nnoremap <leader>cf <cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>
+  nnoremap <leader>fs <cmd>lua require('telescope.builtin').grep_string()<cr>
 ]])
