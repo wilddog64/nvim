dd1ae74ba4c63b5010f3e71dc4455d79fe2444c4
chengkai liang <chengkli@cisco.com>
2022-04-22 09:50:09 -0700

    remove whitespaces

--- lua/usr/options.lua
+++ lua/usr/options.lua
@@ -48,20 +48,20 @@ vim.cmd([[
 
   color Atelier_CaveDark
   if has("autocmd")
-  
+
   " Enable file type detection.
   " Use the default filetype settings, so that mail gets 'tw' set to 72,
   " 'cindent' is on in C files, etc.
   " Also load indent files, to automatically do language-dependent indenting.
   filetype plugin indent on
-  
+
   " Put these in an autocmd group, so that we can delete them easily.
   augroup vimrcEx
   au!
-  
+
   " For all text files set 'textwidth' to 78 characters.
   "autocmd FileType text setlocal textwidth=78
-  
+
   " When editing a file, always jump to the last known cursor position.
   " Don't do it when the position is invalid or when inside an event handler
   " (happens when dropping a file on gvim).
