95806023fe7c8a4e7706d68cc485431dd657982d
chengkai liang <chengkli@cisco.com>
2022-04-30 13:32:43 -0700

    set key maps for cycle through choices

--- lua/usr/luasnip.lua
+++ lua/usr/luasnip.lua
@@ -38,13 +38,13 @@ vim.keymap.set({ "i", "s" }, "<c-p>", function()
   end
 end)
 
-vim.keymap.set({ "i", "s" }, "<a-k>", function()
+vim.keymap.set({ "i", "s" }, "<c-u>", function()
   if ls.choice_active() then
     ls.change_choice(1)
   end
 end)
 
-vim.keymap.set({ "i", "s" }, "<a-h", function()
+vim.keymap.set({ "i", "s" }, "<c-d>", function()
   if ls.choice_active() then
     ls.change_choice(-1)
   end
