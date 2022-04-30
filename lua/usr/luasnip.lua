7a03c9ada5d1ca570dca2e1c334ac5f4a76f2e45
chengkai liang <chengkli@cisco.com>
2022-04-30 13:19:54 -0700

    add key mapping for jumping back and forth

--- lua/usr/luasnip.lua
+++ lua/usr/luasnip.lua
@@ -26,21 +26,19 @@ vim.keymap.set({
 end
 )
 
-vim.keymap.set({
-  "i", "s"
-}, "<a-k>", function()
-  if ls.jummpable(1) then
+vim.keymap.set({ "i", "s" }, "<c-n>", function()
+  if ls.jumpable(1) then
     ls.jump(1)
   end
 end)
 
-vim.keymap.set({ "i", "s" }, "<a-j>", function()
+vim.keymap.set({ "i", "s" }, "<c-p>", function()
   if ls.jumpable(-1) then
     ls.jump(-1)
   end
 end)
 
-vim.keymap.set({ "i", "s" }, "<a-l>", function()
+vim.keymap.set({ "i", "s" }, "<a-k>", function()
   if ls.choice_active() then
     ls.change_choice(1)
   end
