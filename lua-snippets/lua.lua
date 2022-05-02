c72b2c890ffdce6d2b564e5d7dfeab32b3949b58
chengkai liang <chengkli@cisco.com>
2022-05-02 16:08:36 -0700

    add a luh snippet to generate a boilerplate code for writing lua-snippet

--- lua-snippets/lua.lua
+++ lua-snippets/lua.lua
@@ -44,5 +44,32 @@ local myAutoTrigger = s(
 )
 table.insert(autosnippets, myAutoTrigger)
 
-return snippets, autosnippets
+local luaTemplate = s("luh", fmt([[
+local ls = require('luasnip')
+local s = ls.s -- snippet
+local i = ls.i -- insert node
+local t = ls.t -- text node
 
+local d = ls.dynamic_node
+local c = ls.choice_node
+local f = ls.function_node
+local sn = ls.snippet_node
+
+local fmt = require('luasnip.extras.fmt').fmt
+local rep = require('luasnip.extras').rep
+
+-- use double braces to escape {{}}
+local snippets, autosnippets = {{}}, {{}}
+local group = vim.api.nvim_create_augroup('Lua Snippets', {{ clear = true }})
+local file_pattern = "*.lua"
+
+{}
+
+table.insert(snippets, mySnippet)
+return snippets, autosnippets
+]], {
+  i(1, "")
+  }))
+table.insert(snippets, luaTemplate)
+
+return snippets, autosnippets
