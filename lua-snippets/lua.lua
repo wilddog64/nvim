26375eaa72e3666b41ebb9f87a80467649e0b8dc
chengkai liang <chengkli@cisco.com>
2022-05-02 20:23:54 -0700

    add a bolierplate code for creating lua snippet

--- lua-snippets/lua.lua
+++ lua-snippets/lua.lua
@@ -44,5 +44,35 @@ local myAutoTrigger = s(
 )
 table.insert(autosnippets, myAutoTrigger)
 
+local luaTemplate = s("luh", fmt([[
+local ls = require('luasnip')
+local s = ls.s -- snippet
+local i = ls.i -- insert node
+local t = ls.t -- text node
+
+local d = ls.dynamic_node
+local c = ls.choice_node
+local f = ls.function_node
+local sn = ls.snippet_node
+
+local fmt = require('luasnip.extras.fmt').fmt
+local rep = require('luasnip.extras').rep
+
+-- use double braces to escape {{}}
+local snippets, autosnippets = {{}}, {{}}
+local group = vim.api.nvim_create_augroup('Lua Snippets', {{ clear = true }})
+local file_pattern = "*.lua"
+
+{}
+
+table.insert(snippets, {})
+return snippets, autosnippets
+]], {
+  i(1, ""),
+  i(2, ""),
+  }))
+table.insert(snippets, luaTemplate)
+
+
 return snippets, autosnippets
 
