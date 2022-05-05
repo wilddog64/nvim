bc6e0dccc7bf262f531117861cb85558950ca7e7
chengkai liang <chengkli@cisco.com>
2022-05-04 21:23:30 -0700

    remove whitespace in the front

--- lua/usr/luasnip.lua
+++ lua/usr/luasnip.lua
@@ -16,8 +16,8 @@ ls.config.set_config({
   },
 })
 
- require("luasnip/loaders/from_vscode").lazy_load()
- require("luasnip.loaders.from_snipmate").lazy_load()
+require("luasnip/loaders/from_vscode").lazy_load()
+require("luasnip.loaders.from_snipmate").lazy_load()
 
 -- key maps
 vim.keymap.set({
