a336ac7f81a0ca938c647c456eaeef53c4aa1ba7
chengkai liang <chengkli@cisco.com>
2022-05-18 20:52:31 -0700

    add configuration for vim-asterisk

--- init.lua
+++ init.lua
@@ -37,4 +37,5 @@ vim.cmd([[
   source ~/.config/nvim/settings/options.vim
   source ~/.config/nvim/settings/vimux.vim
   source ~/.config/nvim/settings/markdown.vim
+  source ~/.config/nvim/settings/asterisk.vim
 ]])
