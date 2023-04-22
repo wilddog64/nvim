4ffa8f693802940ce532042e002f02fd18dad54d
chengkai liang <chengkai.liang@costcotravel.com>
2023-04-21 20:18:20 -0700

    use . instead of /

--- lua/usr/luasnip.lua
+++ lua/usr/luasnip.lua
@@ -16,7 +16,7 @@ ls.config.set_config({
   },
 })
 
-require("luasnip/loaders/from_vscode").lazy_load()
+require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_snipmate").lazy_load()
 
 -- key maps
