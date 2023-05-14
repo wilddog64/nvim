b6ad8cf5a17f75e64f36c4766bb0c9bb9693eb07
chengkai liang <chengkai.liang@costcotravel.com>
2023-05-14 12:59:22 -0700

    add osc52-nvim plugin and configuration
    add osc52-nvim plugin and configuration
    
    add osc52-nvim plugin and configuration

---
+++ lua/usr/osc52.lua
@@ -0,0 +1,20 @@
+if not vim.fn.exists('+clipboard') then
+  vim.fn.finish()
+end
+local function copy(lines, _)
+  require('osc52').copy(table.concat(lines, '\n'))
+end
+
+local function paste()
+  return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
+end
+
+vim.g.clipboard = {
+  name = 'osc52',
+  copy = {['+'] = copy, ['*'] = copy},
+  paste = {['+'] = paste, ['*'] = paste},
+}
+
+-- Now the '+' register will copy to system clipboard using OSC52
+vim.keymap.set('n', '<leader>c', '"+y')
+vim.keymap.set('n', '<leader>cc', '"+yy')
