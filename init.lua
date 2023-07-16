92ea8e0f4612c23aaf17126ca7583c1290042103
chengkai liang <chengkai.liang@costcotravel.com>
2023-07-16 15:35:52 -0700

    add slim.vim settings

--- init.lua
+++ init.lua
@@ -44,4 +44,5 @@ vim.cmd([[
   source ~/.config/nvim/settings/vim_maximizer.vim
   source ~/.config/nvim/settings/slim.vim
   source ~/.config/nvim/settings/ipy.vim
+  source ~/.config/nvim/settings/slim.vim
 ]])
