d477e8ce9cb6b01c9c67fedb6710e503932a14ed
chengkai liang <chengkai.liang@costcotravel.com>
2023-09-23 06:49:48 -0700

    configure cloudformation

---
+++ lua/usr/lsp/cloudformation.lua
@@ -0,0 +1,15 @@
+-- vim.opt.clipboard = "unnamedplus"
+
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank -i --crlf",
+      ["*"] = "win32yank -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank -o --lf",
+      ["*"] = "win32yank -o --lf",
+    }
+  }
+end
