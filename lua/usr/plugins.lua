b8c960c71f988493ebcec59c33190a7cbdda905f
chengkai liang <ckm.liang@gmail.com>
2024-03-10 14:12:12 -0700

    close nvim tree after file is open

--- lua/usr/plugins.lua
+++ lua/usr/plugins.lua
@@ -159,6 +159,20 @@ return packer.startup(function(use)
   }
 
   use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons'
+    },
+    config = function() require 'nvim-tree'.setup {} end,
+    cmd = { 'NvimTreeToggle' },
+    actions = {
+      open_file = {
+        quit_on_open = true,
+      },
+    },
+  }
+
+  use {
     'SirVer/ultisnips',
     config = function()
       vim.g.UltiSnipsExpandTrigger = '<Plug>(ultisnips_expand)'
