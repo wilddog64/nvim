1c10819d7f8481c6546df26f5a2abf163d0d5a3e
chengkai liang <ckm.liang@gmail.com>
2024-03-10 19:29:39 -0700

    lsp.zero has to happen before lsp-config

--- lua/usr/lsp/init.lua
+++ lua/usr/lsp/init.lua
@@ -1,3 +1,4 @@
+require("usr.lsp.zero")
 local status_ok, lspconfig = pcall(require, "lspconfig")
 if not status_ok then
   return
@@ -5,7 +6,6 @@ end
 
 lspconfig.lua_ls.setup({})
 
-require("usr.lsp.zero")
 -- require "usr.lsp.lsp-installer"
 require("usr.lsp.handlers").setup()
 require "usr.lsp.null-ls"
