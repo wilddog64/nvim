0613827604e2c930863f245867169af1a667ddb5
chengkai liang <chengkai.liang@costcotravel.com>
2024-03-18 19:11:11 -0700

    language servers we like to have

--- lua/usr/lsp/mason.lua
+++ lua/usr/lsp/mason.lua
@@ -8,3 +8,21 @@ require("mason").setup {
 require("mason-lspconfig").setup {
     -- ensure_installed = { "lua-language-server" },
 }
+
+-- MASON LSPCONFIG
+require('mason-lspconfig').setup({
+    ensure_installed = {
+        'azure_pipelines_ls',
+        'bashls',
+        'dockerls',
+        'html',
+        'jdtls',
+        'jsonls',
+        'lua_ls',
+        'marksman',
+        'pylsp',
+        'terraformls',
+        'yamlls',
+    },
+})
+
