8545e54109a0f7ebc92b74563a18b4cd4d95eaaf
chengkai liang <chengkai.liang@costcotravel.com>
2024-03-19 10:48:59 -0700

    add pyls settings

--- lua/usr/lsp/setup.lua
+++ lua/usr/lsp/setup.lua
@@ -102,7 +102,33 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 lspconfig.pylsp.setup({
-  handlers = handlers
+  handlers = handlers,
+  settings = {
+    pyls = {
+      configurationSources = {"flake8"},
+      plugins = {
+        jedi_completion = {enabled = true},
+        jedi_hover = {enabled = true},
+        jedi_references = {enabled = true},
+        jedi_signature_help = {enabled = true},
+        jedi_symbols = {enabled = true, all_scopes = true},
+        pycodestyle = {enabled = false},
+        flake8 = {
+          enabled = true,
+          ignore = {},
+          maxLineLength = 160
+        },
+        mypy = {enabled = false},
+        isort = {enabled = false},
+        yapf = {enabled = false},
+        pylint = {enabled = false},
+        pydocstyle = {enabled = false},
+        mccabe = {enabled = false},
+        preload = {enabled = false},
+        rope_completion = {enabled = false}
+      }
+    }
+  },
 })
 
 lspconfig.terraformls.setup({})
