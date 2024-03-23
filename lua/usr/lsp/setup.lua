1ecd0d02eecc69881cbb2d61228bbdd76f255bb9
chengkai liang <chengkai.liang@costcotravel.com>
2024-03-23 16:47:43 -0700

    update k8s schema setting

--- lua/usr/lsp/setup.lua
+++ lua/usr/lsp/setup.lua
@@ -183,6 +183,7 @@ lspconfig.yamlls.setup {
       schemas = {
         ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.22.0/all.json"] = "/k8s/*.*l",
         ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = {"ci/*.yml", ".gitlab-ci.yml"},
+        ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.16.0-standalone-strict/all.json"] = "/*.y*l",
       },
     }
   }
