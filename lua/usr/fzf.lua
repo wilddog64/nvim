88755691d0cf24fb6fe7b193c4391806e7b7e632
chengkai liang <chengkai.liang@costcotravel.com>
2024-04-26 07:11:35 -0700

    add two more keymap for fzf-lua
    * live grep current buffer
    * list current buffers and switch to the one selected

--- lua/usr/fzf.lua
+++ lua/usr/fzf.lua
@@ -1,5 +1,7 @@
 vim.cmd([[
   " Using Lua functions
+  nnoremap <leader>cf <cmd>lua require('fzf-lua').lgrep_curbuf()<cr>
+  nnoremap <leader>fb <cmd>lua require('fzf-lua').buffers()<cr>
   nnoremap <leader>ff <cmd>lua require('fzf-lua').files()<cr>
   nnoremap <leader>fg <cmd>:lua require('fzf-lua').live_grep_native()<cr>
   nnoremap <leader>fl <cmd>lua require('fzf-lua').loclist()<cr>
