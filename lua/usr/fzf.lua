f928fd6c2cf9e10248b11a7c378337e27b25292c
chengkai liang <ckm.liang@gmail.com>
2024-04-26 06:33:58 -0700

    update keymaps

--- lua/usr/fzf.lua
+++ lua/usr/fzf.lua
@@ -3,6 +3,9 @@ vim.cmd([[
   nnoremap <leader>cf <cmd>lua require('fzf-lua').lgrep_curbuf()<cr>
   nnoremap <leader>fb <cmd>lua require('fzf-lua').buffers()<cr>
   nnoremap <leader>ff <cmd>lua require('fzf-lua').files()<cr>
+  nnoremap <leader>fb <cmd>lua require('fzf-lua').buffers()<cr>
+  nnoremap <leader>fl <cmd>lua require('fzf-lua').lines()<cr>
+  nnoremap <leader>fg <cmd>lua require('fzf-lua').live_grep_native()<cr>
   nnoremap <leader>fl <cmd>lua require('fzf-lua').loclist()<cr>
   nnoremap <leader>fh <cmd>lua require('fzf-lua').tags()<cr>
   inoremap <c-x><c-f> <cmd>lua require("fzf-lua").complete_path()<cr>
