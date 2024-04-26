90f7eac123e5d73dfb124e3609a338de08fea5a6
chengkai liang <ckm.liang@gmail.com>
2024-04-26 05:06:33 -0700

    cleanup fzf-lua key mappings

--- lua/usr/fzf.lua
+++ lua/usr/fzf.lua
@@ -2,16 +2,13 @@ vim.cmd([[
   " Using Lua functions
   nnoremap <leader>cf <cmd>lua require('fzf-lua.builtin').current_buffer_fuzzy_find()<cr>
   nnoremap <leader>fa <cmd>lua require('fzf-lua.builtin').autocommands()<cr>
-  " "nnoremap <leader>fb <cmd>lua require('fzf-lua.builtin').buffers()<cr>
-  nnoremap <leader>fb :Buffers<cr>
+  nnoremap <leader>fb <cmd>lua require('fzf-lua.builtin').buffers()<cr>
   nnoremap <leader>fc <cmd>lua require('telescope.builtin').commands()<cr>
   nnoremap <leader>ff <cmd>lua require('fzf-lua').files()<cr>
   nnoremap <leader>fg <cmd>:lua require('fzf-lua').live_grep_native()<cr>
   nnoremap <leader>fl <cmd>lua require('fzf-lua').loclist()<cr>
   nnoremap <leader>fh <cmd>lua require('fzf-lua').tags()<cr>
   nnoremap <leader>fk <cmd>lua require('fzf-lua.builtin').keymaps()<cr>
-  " nnoremap <leader>fm <cmd>lua require('fzf-lua.builtin').marks()<cr>
-  nnoremap <leader>fm :Marks<cr>
   nnoremap <leader>fr <cmd>lua require('fzf-lua.builtin').registers()<cr>
   nnoremap <leader>fs <cmd>lua require('fzf-lua.builtin').grep_string()<cr>
   nnoremap <leader>ss <cmd>lua require('fzf-lua.builtin').spell_suggest()<cr>
