1859876ab9d96a2888d31cf965ea5209999126fa
chengkai liang <chengkai.liang@costcotravel.com>
2024-10-22 09:24:07 -0700

    disable path completion by using fzf-lua
    as it is really annoying

--- lua/usr/fzf.lua
+++ lua/usr/fzf.lua
@@ -8,6 +8,6 @@ vim.cmd([[
   nnoremap <leader>fg <cmd>lua require('fzf-lua').live_grep_native()<cr>
   nnoremap <leader>fc <cmd>lua require('fzf-lua').loclist()<cr>
   nnoremap <leader>fh <cmd>lua require('fzf-lua').tags()<cr>
-  inoremap <c-x><c-f> <cmd>lua require("fzf-lua").complete_path()<cr>
+  " inoremap <c-x><c-f> <cmd>lua require("fzf-lua").complete_path()<cr>
 ]])
 
