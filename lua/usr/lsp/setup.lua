469be62dfb3e0660b5a5fe835ff65fc60d854909
chengkai liang <ckm.liang@gmail.com>
2024-12-20 20:06:36 -0800

    fix yamlls issues

--- lua/usr/lsp/setup.lua
+++ lua/usr/lsp/setup.lua
@@ -1,14 +1,69 @@
 local lspconfig = require('lspconfig')
 local lsp_defaults = lspconfig.util.default_config
+
+-- TOGGLE INLAY HINTS
+if vim.lsp.inlay_hint then
+    vim.keymap.set('n', '<leader>ih', function()
+        vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled())
+    end, { desc = 'Toggle Inlay Hints' })
+end
+
+local border = {
+    { '┌', 'FloatBorder' },
+    { '─', 'FloatBorder' },
+    { '┐', 'FloatBorder' },
+    { '│', 'FloatBorder' },
+    { '┘', 'FloatBorder' },
+    { '─', 'FloatBorder' },
+    { '└', 'FloatBorder' },
+    { '│', 'FloatBorder' },
+}
+
+-- LSP SETTINGS (FOR OVERRIDING PER CLIENT)
+local handlers = {
+    ['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
+    ['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
+}
+
+-- DIAGNOSTICS SIGNS
+local signs = { Error = ' ', Warn = ' ', Hint = '󰌶 ', Info = ' ' }
+for type, icon in pairs(signs) do
+    local hl = 'DiagnosticSign' .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+vim.diagnostic.config({
+    virtual_text = {
+        prefix = '', -- Could be '●', '▎', │, 'x', '■', , 
+    },
+    float = { border = border },
+    -- virtual_text = false,
+    -- signs = true,
+    -- underline = true,
+})
+
 ---
 -- LSP Servers
 ---
-
 lspconfig.lua_ls.setup({
-  single_file_support = true,
-  flags = {
-    debounce_text_changes = 150,
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT", -- Use LuaJIT for Neovim
+      },
+      diagnostics = {
+        globals = { "vim" }, -- Suppress "undefined global 'vim'" warning
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true), -- Include Neovim runtime
+        checkThirdParty = false, -- Avoid third-party prompts
+      },
+      telemetry = { enable = false }, -- Disable telemetry
+    },
   },
+  on_init = function(client)
+    print("lua-language-server initialized!")
+  end,
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -34,7 +89,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- Jumps to the definition of the type symbol
     bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
 
-    -- Lists all the references 
+    -- Lists all the references
     bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
 
     -- Displays a function's signature information
@@ -58,5 +113,102 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end
 })
 
+lspconfig.pylsp.setup({
+  handlers = handlers,
+  settings = {
+    pyls = {
+      configurationSources = {"flake8"},
+      plugins = {
+        jedi_completion = {enabled = true},
+        jedi_hover = {enabled = true},
+        jedi_references = {enabled = true},
+        jedi_signature_help = {enabled = true},
+        jedi_symbols = {enabled = true, all_scopes = true},
+        pycodestyle = {enabled = false},
+        flake8 = {
+          enabled = true,
+          ignore = {},
+          maxLineLength = 160
+        },
+        mypy = {enabled = false},
+        isort = {enabled = false},
+        yapf = {enabled = false},
+        pylint = {enabled = false},
+        pydocstyle = {enabled = false},
+        mccabe = {enabled = false},
+        preload = {enabled = false},
+        rope_completion = {enabled = false}
+      }
+    }
+  },
+})
 
+lspconfig.terraformls.setup({})
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+  pattern = {"*.tf", "*.tfvars"},
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
+
+lspconfig.bashls.setup({
+  handlers = handlers
+})
+
+lspconfig.marksman.setup({
+  handlers = handlers
+})
+
+lspconfig.dockerls.setup({
+  handlers = handlers
+})
+
+lspconfig.jsonls.setup({
+  handlers = handlers
+})
+
+-- Define a shared root_dir function
+local util = require("lspconfig.util")
+local function get_root_dir(fname)
+    return util.find_git_ancestor(fname) or vim.fn.getcwd()
+end
+
+lspconfig.azure_pipelines_ls.setup {
+  root_dir = get_root_dir,
+  handlers = handlers,
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = {
+          "/azure-pipeline*.y*l",
+          "/*.azure*",
+          "Azure-Pipelines/**/*.y*l",
+          "pipelines/*.y*l",
+        },
+      },
+    },
+  },
+}
+
+lspconfig.yamlls.setup({
+    root_dir = get_root_dir,
+    settings = {
+        yaml = {
+            format = { enable = true },
+            validate = true,
+            schemaStore = {
+                enable = false, -- Disable fetching schemas from SchemaStore
+            },
+            schemas = {
+                -- Explicitly define schemas for Helm files
+                ["https://json.schemastore.org/helmfile.json"] = "helmfile.yaml",
+                ["https://json.schemastore.org/github-workflow.json"] = ".github/workflows/*",
+                ["https://json.schemastore.org/kustomization.json"] = "kustomization.yaml",
+            },
+        },
+    },
+    flags = {
+        debounce_text_changes = 150,
+    },
+})
 
