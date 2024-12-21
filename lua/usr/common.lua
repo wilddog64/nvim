b0eeb6298738cf5784b6a6a00705036fce71720d
chengkai liang <ckm.liang@gmail.com>
2024-12-21 06:42:58 -0800

    move lsp-color setup from usr.common to usr.lsp.config
    make more sense to move lsp relate setup to lsp.config

--- lua/usr/common.lua
+++ lua/usr/common.lua
@@ -1,9 +1,3 @@
 -- common.lua - for lua plugin setup or options that need to be setup
 -- after plugins are installed
 -- simple setup for lsp-colors
-require("lsp-colors").setup({
-  Error = "#db4b4b",
-  Warning = "#e0af68",
-  Information = "#0db9d7",
-  Hint = "#10B981"
-})
--- lua/usr/lsp/config.lua
+++ lua/usr/lsp/config.lua
@@ -9,3 +9,11 @@ vim.diagnostic.config({
 -- hover for diagnostic
 vim.opt.updatetime = 250
 vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(bor, {focus=false, border='rounded', scope='cursor'})]]
+
+require("lsp-colors").setup({
+  Error = "#db4b4b",
+  Warning = "#e0af68",
+  Information = "#0db9d7",
+  Hint = "#10B981"
+})
+
