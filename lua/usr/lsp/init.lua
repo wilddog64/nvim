9942378ffe341b737c95065a4a20c93195ecc666
chengkai liang <ckm.liang@gmail.com>
2024-12-29 05:01:09 -0800

    remove comment out section

--- lua/usr/lsp/init.lua
+++ lua/usr/lsp/init.lua
@@ -10,28 +10,6 @@ if not status_ok then
   return -- Exit gracefully if lspconfig is not installed
 end
 
--- Set up lua-language-server (lua_ls)
--- lspconfig.lua_ls.setup({
---   settings = {
---     Lua = {
---       runtime = {
---         version = "LuaJIT", -- Use LuaJIT for Neovim
---       },
---       diagnostics = {
---         globals = { "vim" }, -- Suppress "undefined global 'vim'" warning
---       },
---       workspace = {
---         library = vim.api.nvim_get_runtime_file("", true), -- Include Neovim runtime
---         checkThirdParty = false, -- Avoid third-party prompts
---       },
---       telemetry = { enable = false }, -- Disable telemetry
---     },
---   },
---   on_init = function(client)
---     print("lua-language-server initialized!")
---   end,
--- })
-
 -- require "usr.lsp.lsp-installer"
 require("usr.lsp.handlers").setup()
 require "usr.lsp.null-ls"
