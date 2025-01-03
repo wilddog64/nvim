761bac9666cc9cc8fc25d53df8168fb5d0e00437
chengkai liang <ckm.liang@gmail.com>
2025-01-02 18:03:16 -0800

    align code

--- lua/usr/python-dap.lua
+++ lua/usr/python-dap.lua
@@ -1,6 +1,6 @@
 local dap = require'dap'
 dap.configurations.python = dap.configurations.python or {}
-    table.insert(dap.configurations.python, {
+table.insert(dap.configurations.python, {
     type = 'python',
     request = 'launch',
     name = 'launch with options',
