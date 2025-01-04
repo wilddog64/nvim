cc28c1690ea29943fb2c7d542b5d0bedd1651954
chengkai liang <ckm.liang@gmail.com>
2025-01-04 15:41:24 -0800

    remove unsed variables

--- lua/usr/lsp/init.lua
+++ lua/usr/lsp/init.lua
@@ -1,15 +1,6 @@
 local lsp_zero = require('lsp-zero')
 lsp_zero.extend_lspconfig()
 
-lsp_zero.on_attach(function(client, bufnr)
-  lsp_zero.default_keymaps({buffer = bufnr})
-end)
-
-local status_ok, lspconfig = pcall(require, 'lspconfig')
-if not status_ok then
-  return -- Exit gracefully if lspconfig is not installed
-end
-
 -- require "usr.lsp.lsp-installer"
 require("usr.lsp.handlers").setup()
 require "usr.lsp.null-ls"
