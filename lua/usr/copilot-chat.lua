c685c10abe87def60397ebf8283f1b7b6d8345e6
chengkai liang <ckm.liang@gmail.com>
2025-01-06 18:56:56 -0800

    convert CopilotChat hotkeys to which-key

--- lua/usr/copilot-chat.lua
+++ lua/usr/copilot-chat.lua
@@ -6,3 +6,12 @@ vim.api.nvim_create_autocmd('FileType', {
         vim.keymap.set('n', '<c-e>', '<cmd>CopilotChatAccept<CR>', { buffer = true })
     end,
 })
+
+local wk = require('which-key')
+
+wk.add({
+  { '<leader>cc', function() vim.cmd([[ CopilotChatToggle ]]) end },
+  { '<leader>cce', function() vim.cmd([[ CopilotChatExplain ]]) end },
+  { '<leader>ccr', function() vim.cmd([[ CopilotChatReview ]]) end },
+  { '<leader>cct', function() vim.cmd([[ CopilotChatTest ]]) end },
+})
