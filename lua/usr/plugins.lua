b0615fba45c533473a4ea37dddfe6eaea81e7cbd
chengkai liang <ckm.liang@gmail.com>
2025-01-06 18:06:07 -0800

    add which-key.nvim plugin

--- lua/usr/plugins.lua
+++ lua/usr/plugins.lua
@@ -281,6 +281,15 @@ use {
     event = 'BufReadPre',
   }
 
+  use {
+    "folke/which-key.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  }
+
   -- LSP
   use {
     'VonHeikemen/lsp-zero.nvim',
