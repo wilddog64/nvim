1791ff1b828f829285fb7ef1a3c3fb29aa04abff
chengkai liang <ckm.liang@gmail.com>
2025-01-07 15:48:51 -0800

    add a group to fzf which-key mappings

--- lua/usr/fzf.lua
+++ lua/usr/fzf.lua
@@ -3,12 +3,12 @@ local wk = require('which-key')
 
 -- add key mappings for various fzf-lua functions
 wk.add({
-  { '<leader>cf', function() require"fzf-lua".lgrep_curbuf() end },
-  { '<leader>ff', function() require"fzf-lua".files() end },
-  { '<leader>fb', function() require"fzf-lua".buffers() end },
-  { '<leader>fl', function() require"fzf-lua".lines() end },
-  { '<leader>fg', function() require"fzf-lua".live_grep_native() end },
-  { '<leader>fc', function() require"fzf-lua".loclist() end },
-  { '<leader>ft', function() require"fzf-lua".tags() end },
+  { '<leader>cf', function() require"fzf-lua".lgrep_curbuf() end, group = 'fzf' },
+  { '<leader>ff', function() require"fzf-lua".files() end, group = 'fzf'  },
+  { '<leader>fb', function() require"fzf-lua".buffers() end, group = 'fzf'  },
+  { '<leader>fl', function() require"fzf-lua".lines() end, group = 'fzf'  },
+  { '<leader>fg', function() require"fzf-lua".live_grep_native() end, group = 'fzf'  },
+  { '<leader>fc', function() require"fzf-lua".loclist() end, group = 'fzf'  },
+  { '<leader>ft', function() require"fzf-lua".tags() end, group = 'fzf'  },
 })
 
