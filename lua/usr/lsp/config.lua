21cf70a8f1ea6904771e1c5fab47b04bd9b7e360
chengkai liang <chengkai.liang@costcotravel.com>
2025-02-08 05:02:59 -0800

    code alignment

--- lua/usr/lsp/config.lua
+++ lua/usr/lsp/config.lua
@@ -6,15 +6,15 @@ vim.diagnostic.config({
   signs = {
     text = {
       [vim.diagnostic.severity.ERROR] = '',
-      [vim.diagnostic.severity.WARN] = '',
-      [vim.diagnostic.severity.INFO] = '',
-      [vim.diagnostic.severity.HINT] = '',
+      [vim.diagnostic.severity.WARN]  = '',
+      [vim.diagnostic.severity.INFO]  = '',
+      [vim.diagnostic.severity.HINT]  = '',
     },
     numhl = {
-      [vim.diagnostic.severity.WARN] = 'WarningMsg',
+      [vim.diagnostic.severity.WARN]  = 'WarningMsg',
       [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
-      [vim.diagnostic.severity.INFO] = 'DiagnosticInfo',
-      [vim.diagnostic.severity.HINT] = 'DiagnosticHint',
+      [vim.diagnostic.severity.INFO]  = 'DiagnosticInfo',
+      [vim.diagnostic.severity.HINT]  = 'DiagnosticHint',
 
     },
   },
