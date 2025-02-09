195deebaa8bd16f7870bd8293dcb6529657d7fc4
chengkai liang <chengkai.liang@costcotravel.com>
2025-02-09 06:54:35 -0800

    load mini.git

--- lua/usr/mini/git.lua
+++ lua/usr/mini/git.lua
@@ -1,24 +1,16 @@
-require('mini.diff').setup({
-  -- Options for how hunks are visualized
-  view = {
-    -- Visualization style. Possible values are 'sign' and 'number'.
-    -- Default: 'number' if line numbers are enabled, 'sign' otherwise.
-    style = vim.go.number and 'number' or 'sign',
+require('mini.git').setup({
+  -- General CLI execution
+  job = {
+    -- Path to Git executable
+    git_executable = 'git',
 
-    -- Signs used for hunks with 'sign' view
-    signs = { add = '+', change = '~', delete = '-' },
-
-    -- Priority of used visualization extmarks
-    priority = 199,
+    -- Timeout (in ms) for each job before force quit
+    timeout = 30000,
   },
 
-  -- Source for how reference text is computed/updated/etc
-  -- Uses content from Git index by default
-  source = nil,
-
-  -- Delays (in ms) defining asynchronous processes
-  delay = {
-    -- How much to wait before update following every text change
-    text_change = 200,
+  -- Options for `:Git` command
+  command = {
+    -- Default split direction
+    split = 'auto',
   },
 })
