4fe3fd51bab5db00fcaf82716639bd8a1a488a9d
chengkai liang <chengkai.liang@costcotravel.com>
2025-03-08 13:28:52 -0800

    remove ultisnips

--- lua/usr/lazy.lua
+++ lua/usr/lazy.lua
@@ -265,18 +265,6 @@ require('lazy').setup({
   },
 
   {
-    'SirVer/ultisnips',
-    config = function()
-      vim.g.UltiSnipsExpandTrigger = '<Plug>(ultisnips_expand)'
-      vim.g.UltiSnipsJumpForwardTrigger = '<Plug>(ultisnips_jump_forward)'
-      vim.g.UltiSnipsJumpBackwardTrigger = '<Plug>(ultisnips_jump_backward)'
-      vim.g.UltiSnipsListSnippets = '<c-x><c-s>'
-      vim.g.UltiSnipsRemoveSelectModeMappings = 0
-    end,
-    event = 'BufReadPre',
-  },
-
-  {
     'ibhagwan/fzf-lua',
     dependencies = {
       'nvim-tree/nvim-web-devicons',
