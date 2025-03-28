357b3a51947b8f922dd5c123c14de6e50f8866b5
chengkai liang <chengkai.liang@costcotravel.com>
2025-03-27 18:55:36 -0700

    make osc52 and win32yank modules into clipboard.lua

---
+++ lua/usr/clipboard.lua
@@ -0,0 +1,33 @@
+if not vim.fn.exists('+clipboard') then
+  return
+end
+
+local function copy(lines, _)
+  require('osc52').copy(table.concat(lines, '\n'))
+end
+
+local function paste()
+  return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
+end
+
+-- vim.opt.clipboard = "unnamedplus"
+if vim.fn.executable('win32yank.exe') == 0 then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    }
+  }
+else
+  vim.g.clipboard = {
+    name = 'osc52',
+    copy = {['+'] = copy, ['*'] = copy },
+    paste = {['+'] = paste, ['*'] = paste },
+  }
+end
+
