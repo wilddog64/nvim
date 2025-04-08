5806ad35d36cc00e3875323a76575535c0f587a1
chengkai liang <chengkai.liang@costcotravel.com>
2025-04-08 16:54:20 -0700

    remove ultisnips plugin

--- lua/usr/lazy.lua
+++ lua/usr/lazy.lua
@@ -264,17 +264,17 @@ require('lazy').setup({
     }
   },
 
-  {
-    'SirVer/ultisnips',
-    config = function()
-      vim.g.UltiSnipsExpandTrigger = '<Plug>(ultisnips_expand)'
-      vim.g.UltiSnipsJumpForwardTrigger = '<Plug>(ultisnips_jump_forward)'
-      vim.g.UltiSnipsJumpBackwardTrigger = '<Plug>(ultisnips_jump_backward)'
-      vim.g.UltiSnipsListSnippets = '<c-x><c-s>'
-      vim.g.UltiSnipsRemoveSelectModeMappings = 0
-    end,
-    event = 'BufReadPre',
-  },
+--  {
+--    'SirVer/ultisnips',
+--    config = function()
+--      vim.g.UltiSnipsExpandTrigger = '<Plug>(ultisnips_expand)'
+--      vim.g.UltiSnipsJumpForwardTrigger = '<Plug>(ultisnips_jump_forward)'
+--      vim.g.UltiSnipsJumpBackwardTrigger = '<Plug>(ultisnips_jump_backward)'
+--      vim.g.UltiSnipsListSnippets = '<c-x><c-s>'
+--      vim.g.UltiSnipsRemoveSelectModeMappings = 0
+--    end,
+--    event = 'BufReadPre',
+--  },
 
   {
     'ibhagwan/fzf-lua',
