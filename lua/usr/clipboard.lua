26825d5760b60c77e6c249fa60b31e2df0fec263
chengkai liang <chengkai.liang@costcotravel.com>
2025-04-08 10:53:08 -0700

    refactor clipboard

--- lua/usr/clipboard.lua
+++ lua/usr/clipboard.lua
@@ -8,37 +8,40 @@ if not vim.fn.exists('+clipboard') then
   return
 end
 
--- Check to see if win32yank is available
-if vim.fn.executable('win32yank.exe') == 1 then
-  vim.g.clipboard = {
-    name = "win32yank-wsl",
-    copy = {
-      ["+"] = "win32yank.exe -i --crlf",
-      ["*"] = "win32yank.exe -i --crlf",
-    },
-    paste = {
-      ["+"] = "win32yank.exe -o --lf",
-      ["*"] = "win32yank.exe -o --lf",
-    }
-  }
-  vim.notify('Clipboard set to win32yank', vim.log.levels.INFO)
-else -- else we use osc52
-  -- copy and paste are functions that handle the copying and pasting of text
-  -- using the `osc52` plugin. The `copy` function takes a table of lines and
-  -- concatenates them into a single string,
-  local function copy(lines, _)
-    require('osc52').copy(table.concat(lines, '\n'))
-  end
-
-  local function paste()
-    return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
-  end
-
-  vim.g.clipboard = {
-    name = 'osc52',
-    copy = {['+'] = copy, ['*'] = copy },
-    paste = {['+'] = paste, ['*'] = paste },
-  }
-  vim.notify('Clipboard set to osc52', vim.log.levels.INFO)
+local use_win32yank = vim.fn.executable('win32yank.exe') and not vim.fn.has('wsl')
+if use_win32yank then
+  vim.notify('Using win32yank for clipboard operations', vim.log.levels.INFO)
+else
+  vim.notify('Using osc52 for clipboard operations', vim.log.levels.INFO)
 end
 
+local function copy(lines, _)
+  local content = table.concat(lines, '\n')
+  if use_win32yank then
+    local handle = io.popen('win32yank.exe -i --crlf', 'w')
+    if handle then
+      handle:write(content)
+      handle:close()
+    else
+      vim.notify('copy content' .. content .. 'via osc52')
+      require('osc52').copy(table.concat(lines, '\n'))
+    end
+  else
+    require('osc52').copy(content)
+  end
+end
+
+local function paste()
+  if use_win32yank then
+    local output = vim.fn.systemlist('win32yank.exe -o --lf')
+    return { output, 'v' }
+  else
+    return { vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('') }
+  end
+end
+
+ vim.g.clipboard = {
+   name = use_win32yank and 'win32yank' or 'osc52',
+   copy = {['+'] = copy, ['*'] = copy },
+   paste = {['+'] = paste, ['*'] = paste },
+ }
