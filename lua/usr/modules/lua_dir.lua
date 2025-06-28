c7eed2da9ca41751f8bcd83f429091fc06cc3421
chengkai liang <chengkai.liang@costcotravel.com>
2025-06-28 16:37:19 -0700

    move hotkey for get_lua_config_dir into its module

--- lua/usr/modules/lua_dir.lua
+++ lua/usr/modules/lua_dir.lua
@@ -45,4 +45,24 @@ M.get_lua_config_dir = function()
   return ""
 end
 
+function M.enable_keymap()
+  local group = vim.api.nvim_create_augroup("luamap", { clear = true })
+  vim.api.nvim_create_autocmd("BufEnter", {
+    group = group,
+    pattern = "*.lua",
+    callback = function(event)
+      local bufnr = event.buf
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "gf", "", {
+        silent = true,
+        callback = function()
+          local path = M.get_lua_config_dir()
+          if path and path ~= "" then
+            vim.cmd("edit " .. path)
+          end
+        end,
+      })
+    end,
+  })
+end
+
 return M
