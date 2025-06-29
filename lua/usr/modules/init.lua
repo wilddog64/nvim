01d8f29cf6a4f6df03b68902c1a263ceb6c7fc0f
chengkai liang <chengkai.liang@costcotravel.com>
2025-06-28 17:37:05 -0700

    rename preserve_status to preseve_state
    rename preserve_status to preseve_state

--- lua/usr/modules/init.lua
+++ lua/usr/modules/init.lua
@@ -1,5 +1,5 @@
 local lua_dir = require('usr.modules.lua_dir')
-local preserve_state = require('usr.modules.preserve_status')
+local preserve_state = require('usr.modules.preserve_state')
 
 return {
   enable_lua_dir_keymap = lua_dir.enable_keymap,
