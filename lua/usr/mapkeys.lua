6270ea08c93700915cb4353575a75c3aee199f5e
chengkai liang <chengkai.liang@costcotravel.com>
2025-07-02 06:07:32 -0700

    move gundo hotkey to use which-key
    remove gundo.vim setting

--- lua/usr/mapkeys.lua
+++ lua/usr/mapkeys.lua
@@ -1,17 +1,17 @@
 local wk = require('which-key')
+vim.g.gundo_prefer_python3 = 1   -- Lua init
 
 -- add key mappings for LazyGit
 wk.add({
   mode = { 'n', 'i' },
   { '<leader>gi', function() vim.cmd([[ LazyGit ]]) end },
+  { '<leader>gu', ':GundoToggle<CR>', desc = 'Toggle Gundo' },
 })
 
 wk.add({
   mode = { 'i' },
   { '<tab>', '<Plug>(copilot-accept-word)' },
   { '<c-e>', '<Plug>(copilot-accept-line)' },
-})
-
-wk.add({
   { '<leader>tw', function() require('mini.trailspace').trim() end },
 })
+
