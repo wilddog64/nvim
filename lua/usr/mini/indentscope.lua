426def6739de94be01bfd4cef9ac96d61b986b3f
chengkai liang <chengkai.liang@costcotravel.com>
2025-07-02 17:51:09 -0700

    make mini.indentscope work with different file types

--- lua/usr/mini/indentscope.lua
+++ lua/usr/mini/indentscope.lua
@@ -1,7 +1,43 @@
-require('mini.indentscope').setup({
-  options = {
+-- require('mini.indentscope').setup({
+--   options = {
+--     try_as_border = true,
+--     border = 'both',
+--     indent_at_cursor = false,
+--   }
+-- })
+
+-- Custom configuration for mini.indentscope
+local M = {}
+
+function M.setup()
+  -- Default configuration for all file types
+  require('mini.indentscope').setup({
     try_as_border = true,
-    border = 'top',
+    border = 'both',
     indent_at_cursor = false,
-  }
-})
+  })
+
+  -- Create autocmd group for YAML-specific settings
+  local augroup = vim.api.nvim_create_augroup('MiniIndentscopeConfig', { clear = true })
+
+  -- Configure special settings for YAML files
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'yaml',
+    group = augroup,
+    callback = function()
+
+      -- Since we can't modify internal config directly
+      require('mini.indentscope').setup({
+        symbol = '│',
+        options = {
+          try_as_border = true,
+          border = 'top',  -- YAML-specific setting
+          indent_at_cursor = false,
+        }
+      })
+    end
+  })
+end
+
+return M
+
