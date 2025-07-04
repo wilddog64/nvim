725d2b7748fededceb750cf4195a6ba76c04649b
chengkai liang <chengkai.liang@costcotravel.com>
2025-07-04 05:39:45 -0700

    move trim whitespaces into insert, and norm modes

--- lua/usr/mapkeys.lua
+++ lua/usr/mapkeys.lua
@@ -6,12 +6,12 @@ wk.add({
   mode = { 'n', 'i' },
   { '<leader>gi', function() vim.cmd([[ LazyGit ]]) end },
   { '<leader>gu', ':GundoToggle<CR>', desc = 'Toggle Gundo' },
+  { '<leader>tw', function() require('mini.trailspace').trim() end },
 })
 
 wk.add({
   mode = { 'i' },
   { '<tab>', '<Plug>(copilot-accept-word)' },
   { '<c-e>', '<Plug>(copilot-accept-line)' },
-  { '<leader>tw', function() require('mini.trailspace').trim() end },
 })
 
